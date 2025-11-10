f575024f056fdc1d3d94e4589479ef7e92cfb060
Ladislav Slezák <lslezak@suse.com>
2025-11-10 18:19:45 +0100

    Updated notes

--- src/app/Notes.tsx
+++ src/app/Notes.tsx
@@ -17,18 +17,18 @@ export default function Notes(): React.ReactNode {
           The profile is validated directly in your browser, no data is sent outside of your machine. It is safe to
           include passwords, registration keys or similar sensitive data in the validated profile.
         </Content>
-        { new URL(document.URL).protocol !== "file:" && <Content component={ContentVariants.li}>
+        <Content component={ContentVariants.li}>
           The editor is quite powerful, it supports highlighting and code completion (Ctrl+Space), it uses the schema
           definition to offer the possible values. The attribute description is displayed on mouse hover. To navigate
           between the found problems press F8. More actions can be found in the context menu (right mouse click) or in
           the command pallette (F1 key).
-        </Content>}
+        </Content>
         {
-          // do not display when running locally, the link only works from the github pages build
+          // do not display when running locally, the link only works from the GitHub pages build
           new URL(document.URL).hostname.endsWith(".github.io") && (
             <Content component={ContentVariants.li}>
-              You can download the <a href="./agama-validator.zip">offline validator version</a> and use the validator
-              locally even without any internet connection.
+              You can download the <a href="./agama-validator.zip">validator ZIP archive</a> and deploy the validator
+              locally.
             </Content>
           )
         }
