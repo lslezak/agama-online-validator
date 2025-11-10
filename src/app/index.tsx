65e50d1e9e6e017963e3290525edbf9029106cd1
Ladislav Slezák <lslezak@suse.com>
2025-11-10 20:11:01 +0100

    Show custom install button

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,8 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import "@patternfly/react-core/dist/styles/base.css";
 import "@app/app.css";
 import {
+  Button,
   Form,
   FormGroup,
   Masthead,
@@ -13,7 +14,10 @@ import {
   ToolbarContent,
   ToolbarGroup,
   ToolbarItem,
+  Tooltip,
 } from "@patternfly/react-core";
+
+import DownloadIcon from "@patternfly/react-icons/dist/esm/icons/download-icon";
 
 import ProfileEditor from "./ProfileEditor";
 import Notes from "./Notes";
@@ -22,12 +26,28 @@ import { THEME_TYPES, useTheme } from "./useTheme";
 import { ProfileSelector } from "./ProfileSelector";
 import { SchemaDefinition } from "./schemaFetcher";
 
+// the BeforeInstallPromptEvent is non-standard and not defined in the Typescript DOM library
+interface BeforeInstallPromptEvent extends Event {
+  readonly platforms: string[];
+  readonly userChoice: Promise<{
+    outcome: "accepted" | "dismissed";
+    platform: string;
+  }>;
+  prompt(): Promise<void>;
+}
+
+declare global {
+  interface WindowEventMap {
+    beforeinstallprompt: BeforeInstallPromptEvent;
+  }
+}
+
 const App: React.FunctionComponent = () => {
   const theme = useTheme(THEME_TYPES.COLOR);
   const [isDarkTheme, setIsDarkTheme] = useState(
     theme.mode === theme.modes.DARK || (theme.mode === theme.modes.SYSTEM && theme.resolvedTheme === theme.modes.DARK),
   );
-
+  const [installPrompt, setInstallPrompt] = useState<BeforeInstallPromptEvent>();
   const [schema, setSchema] = useState<SchemaDefinition[]>([]);
 
   const themeChanged = useCallback(
@@ -41,12 +61,52 @@ const App: React.FunctionComponent = () => {
     setSchema(loadedSchema);
   }, []);
 
+  // handle events related to PWA installation
+  useEffect(() => {
+    const beforeInstall = function (event: BeforeInstallPromptEvent) {
+      event.preventDefault();
+      setInstallPrompt(event);
+    };
+    window.addEventListener("beforeinstallprompt", beforeInstall);
+
+    const appInstalled = function () {
+      setInstallPrompt(undefined);
+    };
+    window.addEventListener("appinstalled", appInstalled);
+
+    // cleanup callback to unregister the event listeners
+    return () => {
+      window.removeEventListener("beforeinstallprompt", beforeInstall);
+      window.removeEventListener("appinstalled", appInstalled);
+    };
+  });
+
+  const installApp = function () {
+    if (installPrompt) installPrompt.prompt();
+  };
+
   const masthead = (
     <Masthead>
       <MastheadContent>
         <Toolbar>
           <ToolbarGroup align={{ default: "alignEnd" }}>
             <ToolbarContent>
+              {installPrompt && (
+                <ToolbarItem>
+                  <Tooltip
+                    content={
+                      <div>
+                        Install the editor as a local application. The application icon is placed on the desktop and the
+                        JSON file extension is associated with the editor.
+                      </div>
+                    }
+                  >
+                    <Button variant="control" icon={<DownloadIcon />} onClick={installApp}>
+                      Install app
+                    </Button>
+                  </Tooltip>
+                </ToolbarItem>
+              )}
               <ToolbarItem>
                 <ThemeSwitcher theme={theme} themeChanged={themeChanged} />
               </ToolbarItem>
